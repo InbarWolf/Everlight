49eb32757a06ed74b70ca521174ddb7d801031ad
Inbar <35798696+InbarWolf@users.noreply.github.com>
2018-01-28 09:00:13 +1100

    Add files via upload
    Coding Project
    Write a console app that models the system on the next slide.
    You should first initialise your system passing in the depth of the system – in this case 4. 
    Set the state of each gate switch randomly to left or right.
    Model 2 behaviours:
    Predict the answer to the solution i.e which container does not receive a ball.
    Run the model to completion and check that your prediction matches the
    outcome.
    At each intersection is a gate.
    When a ball passes a gate it will pass down the open side, but then the gate will move to the other side.
    There are 15 balls and 16 containers. Which container will not receive a ball when all balls have passed through the system.

---
+++ TreePinBallApplication/TreePinBallApplication/Gate.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreePinBallApplication
+{
+    
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+
+    public class Gate
+    {
+        #region Members
+
+        public static int StaticGateID;
+
+        private string gateID;
+        private int gateLevel;
+        private Gate leftGate;
+        private Gate rightGate;
+        private Direction openDirection;
+
+        #endregion
+
+
+        #region Public Functions
+        public Gate(bool isRoot = false)
+        {
+            if (isRoot)
+                Gate.StaticGateID = 0;
+
+            Random rand = new Random(Guid.NewGuid().GetHashCode());
+            openDirection = ((rand.Next() % 2) == 0) ? Direction.Left : Direction.Right;
+        }
+
+        public void BuildTree(int level)
+        {
+            gateLevel = level;
+            if (level > 0)
+            {
+                leftGate = new Gate();
+                leftGate.BuildTree(level - 1);
+                rightGate = new Gate();
+                rightGate.BuildTree(level - 1);
+            }
+            else
+            {
+                gateID = GateIndex2AlphabeticalString(++StaticGateID, true);
+            }
+        }
+
+        public string ReciveBall()
+        {
+            string gatesWithBalls;
+            if (gateLevel == 0)
+            {
+                gatesWithBalls = gateID + ",";
+            }
+            else
+            {
+                if (openDirection == Direction.Left)
+                    gatesWithBalls = leftGate.ReciveBall();
+                else
+                    gatesWithBalls = rightGate.ReciveBall();
+            }
+
+            ChangeGateDirection();
+            return gatesWithBalls;
+        }
+        #endregion
+
+        #region Private Functions
+        private void ChangeGateDirection()
+        {
+            if (openDirection == Direction.Left)
+                openDirection = Direction.Right;
+            else
+                openDirection = Direction.Left;
+        }
+
+        private static string GateIndex2AlphabeticalString(int number, bool isCaps)
+        {
+            string aphabeticalNumber = string.Empty;
+
+            int prefixNum = number / 27;
+
+            if (prefixNum > 0)
+            {
+                aphabeticalNumber = ((Char)(64 + prefixNum)).ToString();
+                aphabeticalNumber += ((Char)(65 + (number - (27 * prefixNum)))).ToString();
+            }
+            else
+            {
+                aphabeticalNumber += ((Char)(64 + number)).ToString();
+            }
+
+            return aphabeticalNumber;
+        }
+
+    } 
+    #endregion
+}
---
+++ TreePinBallApplication/TreePinBallApplication/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TreePinBallApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string systemDepth;
+
+                Console.WriteLine("Please enter the depth of your system");
+
+                Regex regex = new Regex("^[0-9]+$");
+                while (true)
+                {
+                    systemDepth = Console.ReadLine();
+                    if (regex.Match(systemDepth).Success)
+                        break;
+
+                    Console.WriteLine("Please enter digits only");
+                }
+
+                Play(int.Parse(systemDepth));
+
+                Console.WriteLine("Would you like to play again? (Y)");
+                string input = Console.ReadLine();
+                if (input != "Y")
+                    break;
+
+                
+            }
+
+        }
+
+        private static void Play(int systemDepth)
+        {
+            double numberOfBalls = Math.Pow(2, systemDepth) - 1;
+
+            Gate tree = new Gate(true);
+            tree.BuildTree(systemDepth);
+
+            string Winningates = string.Empty;
+
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                Winningates += tree.ReciveBall();
+            }
+
+            Console.WriteLine(string.Format("The Gates that received a ball {0}", Winningates.TrimEnd(',')));
+        }
+    }
+}
